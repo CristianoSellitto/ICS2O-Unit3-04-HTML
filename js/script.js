30ae78dabb6219b1b0c58fbc47668cd3a009968b
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-06 17:01:44 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit3-04-HTML
+// April 6 2022
 
 'use strict'
 /**
- * Alerts "Hello, World!"
+ * Calculates fahrenheit to celcius and celcius to fahrenheit using the user's input. 
  */
-function onButtonClick() {
-  alert("Hello, World!")
+function calculate(input) {
+  if (input == 'fahrenheit') { // If the input is fahrenheit, do fahrenheit to celcius.
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+    const answer = ((fahrenheit - 32) * (5/9)).toFixed(2)
+    document.getElementById("output-celsius").innerHTML = "<p>This temperature is equal to " + answer + "°C</p>"
+  } else if (input == 'celcius') { // If the input is celcius, do celcius to fahrenheit.
+    const celcius = parseFloat(document.getElementById("celcius").value)
+    const answer = ((celcius * (9/5)) + 32).toFixed(2)
+    document.getElementById("output-fahrenheit").innerHTML = "<p>This temperature is equal to " + answer + "°F</p>"
+  }
 }
