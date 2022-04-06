6e303b53313552469ae1f2851ed75c0a51006ddd
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-06 17:03:15 +0000

    Fixed typo

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@
 
 'use strict'
 /**
- * Calculates fahrenheit to celcius and celcius to fahrenheit using the user's input. 
+ * Calculates fahrenheit to celsius and celsius to fahrenheit using the user's input. 
  */
 function calculate(input) {
-  if (input == 'fahrenheit') { // If the input is fahrenheit, do fahrenheit to celcius.
+  if (input == 'fahrenheit') { // If the input is fahrenheit, do fahrenheit to celsius.
     const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
     const answer = ((fahrenheit - 32) * (5/9)).toFixed(2)
     document.getElementById("output-celsius").innerHTML = "<p>This temperature is equal to " + answer + "°C</p>"
-  } else if (input == 'celcius') { // If the input is celcius, do celcius to fahrenheit.
-    const celcius = parseFloat(document.getElementById("celcius").value)
-    const answer = ((celcius * (9/5)) + 32).toFixed(2)
+  } else if (input == 'celsius') { // If the input is celsius, do celsius to fahrenheit.
+    const celsius = parseFloat(document.getElementById("celsius").value)
+    const answer = ((celsius * (9/5)) + 32).toFixed(2)
     document.getElementById("output-fahrenheit").innerHTML = "<p>This temperature is equal to " + answer + "°F</p>"
   }
 }
